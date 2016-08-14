16b0ad43db5f1cbddf999bd1129726f353020464
Skyler <skyler.m.degrote@outlook.com>
2016-08-14 18:13:25 -0500

    adding mobile friendly menu

--- includes/navigation.php
+++ includes/navigation.php
@@ -6,14 +6,62 @@
 </head>
 <body>
 <nav>
-    <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li></li>
-        <li class="alignRight">
+    <div class="sitenavigation">
+	<span class="menu-icon">
+		<span class="line"></span>
+		<span class="line"></span>
+		<span class="line"></span>
+	</span>
 
-        </li>
-    </ul>
+        <ul>
+            <li><a href="#">First Item</a></li>
+            <li class="nav-dropdown"><a href="#">Second Item</a>
+                <ul>
+                    <li><a href="#">First Sub-nav item</a></li>
+                    <li><a href="#">Second Sub-nav item</a></li>
+                    <li class="nav-dropdown"><a href="#">Third Sub-nav item</a>
+                        <ul>
+                            <li><a href="#">Third level nav item</a></li>
+                            <li><a href="#">Third level nav item</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="#">Forth Sub-nav item</a></li>
+                </ul>
+            </li>
+            <li class="nav-dropdown"><a href="#">Third Item</a>
+                <ul>
+                    <li><a href="#">Nav item</a></li>
+                    <li class="nav-dropdown"><a href="#">Nav item</a>
+                        <ul>
+                            <li><a href="#">Nav item</a></li>
+
+                        </ul>
+                    </li>
+                    <li class="nav-dropdown"><a href="#">Third Sub-nav item</a>
+                        <ul>
+                            <li><a href="#">Third level nav item</a></li>
+
+                        </ul>
+                    </li>
+
+                    <li><a href="#">Forth Sub-nav item</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Forth Item</a></li>
+        </ul>
+    </div>
+<!--    <ul>-->
+<!--        <li>Home</li>-->
+<!--        <li>About</li>-->
+<!--        <li>Resources</li>-->
+<!--        <li>How To</li>-->
+<!--        <li>Blog</li>-->
+<!--        <li class="alignRight">Members</li>-->
+<!--        <li class="alignRight">-->
+<!--            Log In-->
+<!--        </li>-->
+<!--    </ul>-->
 </nav>
 </body>
 </html>
