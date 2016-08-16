e753d38d9258559c3d6c0c79b242a733899d2aac
Skyler <skyler.m.degrote@outlook.com>
2016-08-16 18:23:12 -0500

    adding grid for responsive layout

---
+++ grid.php
@@ -0,0 +1,73 @@
+<!doctype>
+    <html>
+<head>
+    <link rel="stylesheet" href="styles/grid.css">
+</head>
+<body>
+<div class="grid">
+    <div class="grid__item grid__item--xs-span-1">
+        xs-1
+    </div>
+    <div class="grid__item grid__item--xs-span-2">
+        xs-2
+    </div>
+    <div class="grid__item grid__item--xs-span-3">
+        xs-3
+    </div>
+    <div class="grid__item grid__item--xs-span-6">
+        xs-6
+    </div>
+</div>
+
+<div class="grid">
+    <div class="grid__item grid__item--md-span-4">
+        md-4
+    </div>
+    <div class="grid__item grid__item--md-span-4">
+        md-4
+    </div>
+    <div class="grid__item grid__item--md-span-4">
+        md-4
+    </div>
+</div>
+
+<div class="grid">
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+    <div class="grid__item grid__item--sm-span-1">
+        sm-1
+    </div>
+</div>
+</body>
+</html>
